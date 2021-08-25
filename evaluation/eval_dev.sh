fa79797c21cf4b6ec68bb7d201e95fd3bd79eba4
Wei-Peng <30322673+a414351664@users.noreply.github.com>
2021-08-25 15:15:17 +0800

    Update eval_dev.sh

--- evaluation/eval_dev.sh
+++ evaluation/eval_dev.sh
@@ -1,7 +1,7 @@
 # source ~/.bashrc
-INPUT_FILE=~/CommonGen-plus/dataset/final_data/commongen/commongen.dev.src_alpha.txt
-TRUTH_FILE=~/CommonGen-plus/dataset/final_data/commongen/commongen.dev.tgt.txt
-PRED_FILE=$1
+INPUT_FILE=~/data1/Commongen/final_data/commongen/commongen.dev.src_alpha.txt
+TRUTH_FILE=~/data1/Commongen/final_data/commongen/commongen.dev.tgt.txt
+PRED_FILE=~/data1/Bang_commongen/outputs/sort_hypo_glge_xsum_ck20_bsz1.txt.dedup
 
 echo ${INPUT_FILE} 
 echo ${TRUTH_FILE}
@@ -9,18 +9,18 @@ echo ${PRED_FILE}
 
 echo "Start running ROUGE"
 
-cd ~/CommonGen-plus/methods/unilm_based
-~/anaconda3/envs/unilm_env/bin/python unilm/src/gigaword/eval.py --pred ${PRED_FILE}   --gold ${TRUTH_FILE} --perl
+cd unilm
+python src/gigaword/eval.py --pred ${PRED_FILE}  --gold ${TRUTH_FILE} --perl
 
 
 echo "BLEU/METER/CIDER/SPICE"
-cd ~/CommonGen/evaluation/Traditional/eval_metrics/
+cd ~/EKI-BART/TRAB-IKE/evaluation/Traditional/eval_metrics/
 ~/anaconda3/envs/coco_score/bin/python eval.py --key_file ${INPUT_FILE} --gts_file ${TRUTH_FILE} --res_file ${PRED_FILE}
 
 
 echo "Coverage"
-cd ~/CommonGen-plus/evaluation/PivotScore
-~/anaconda3/envs/pivot_score/bin/python evaluate.py --pred ${PRED_FILE}   --ref ${TRUTH_FILE} --cs ${INPUT_FILE} --cs_str ../../dataset/final_data/commongen/commongen.dev.cs_str.txt
+cd ~/EKI-BART/TRAB-IKE/evaluation/PivotScore
+~/anaconda3/envs/pivot_score/bin/python evaluate.py --pred ${PRED_FILE}   --ref ${TRUTH_FILE} --cs ${INPUT_FILE} --cs_str ~/data1/Commongen/final_data/commongen/commongen.dev.cs_str.txt
 
 
 
